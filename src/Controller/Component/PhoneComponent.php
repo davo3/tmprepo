223eb0006adbdb3f4ce2bbac20113d97cbe1351c
root <hashmodecom@gmail.com>
2016-04-03 16:10:49 +0100

    add data

---
+++ src/Controller/Component/PhoneComponent.php
@@ -0,0 +1,16 @@
+<?php
+namespace Tmprepo\Controller\Component;
+
+use Cake\Controller\Component;
+
+/**
+ * Phone Component
+ */
+class PhoneComponent extends Component
+{
+
+    public function getNumber() {
+    	return '555';
+    }
+
+}
